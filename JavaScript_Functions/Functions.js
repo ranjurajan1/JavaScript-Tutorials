bdfe8c72861b35b7e60f9e0d403bfd1b5726f6cb
ranjurajan1 <ranju.rnj123@gmail.com>
2026-06-05 22:31:52 +0530

    JavaScript Functions

--- JavaScript_Functions/Functions.js
+++ JavaScript_Functions/Functions.js
@@ -1,11 +1,13 @@
 // NORMAL FUNCTION EXAMPLE 1
-
+//------------------------
 // function JuiceType() // Create a function 
 // {
 //     console.log("Lemon juice");
 // }
 // JuiceType();  // Function calling
 // // Output : Lemon juice 
+
+//-----------------------
 
 //NORMAL FUNCTION EXAMPLE 2
 
@@ -16,6 +18,7 @@
 // hobbies() // here invokes or call the function
 // Output : Photography 
 
+//-----------------------
 
 //NORMAL FUNCTION EXAMPLE 3
 
@@ -26,8 +29,23 @@
 // FavDress();
 //Output : Skirt
 
+//-----------------------
+//NORMAL FUNCTION EXAMPLE 3
+// function nameOfTheFunction()
+// {
+//     console.log("Woooh");
+//     console.log("I am learning JavaScript")
+// }
+// nameOfTheFunction();
+// Woooh I am learning JavaScript
+
+//--------------------------------
+
 //FUNCTION USING PARAMETERS
+//***************************//
+
 //EXAPMLE 1
+
 //we can give an input from outside
 // function heloparayuka(name) // here name is a parameter
 // {
@@ -37,38 +55,50 @@
 // heloparayuka("appu");
 // heloparayuka("Ranju");
 
-// //-------------------------------
-// //EXAPMLE 12
+//-----------------------
+
+// //EXAPMLE 2
+
 // function kuttuka(a,b)
 // {
 //     console.log(a+b);
 // }
 // kuttuka(5,10);
-//------------------
-//EXAPMLE 3
 
+//-----------------------
+
+//EXAPMLE 3
 
 // function kuttuka(a,b)
 // {
 //     console.log();
 // }
 // kuttuka(5,10);
-// //-------
+
+//-----------------------
+
+//EXAPMLE 4
+
 // function enteage(age)
 // {
 // console.log("I am" + age);
 // }
 // enteage(20);
-//-------------
+
+//-----------------------
+//EXAPMLE 5
+
 // function vehicle(bike,car,lory)
 // {
 //     console.log("enik" +lory);
 // }
 // vehicle("a", "bullet", "taxi");
 
+//----------------------
+//EXAPMLE 6
 
-function vehicle(bike,car,lory,bus)
-{
-    console.log("enik" +bus + "ishttom");
-}
-vehicle("null", "null", "nayikurukkn","xyx");
+// function vehicle(bike,car,lory,bus)
+// {
+//     console.log("enik" +bus + "ishttom");
+// }
+// vehicle("null", "null", "nayikurukkn","xyx");
