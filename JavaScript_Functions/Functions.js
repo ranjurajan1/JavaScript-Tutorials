fb22e9c961efec0c98953de5b694c498857a0427
ranjurajan1 <ranju.rnj123@gmail.com>
2026-06-08 21:21:37 +0530

    function using parameters

--- JavaScript_Functions/Functions.js
+++ JavaScript_Functions/Functions.js
@@ -15,7 +15,7 @@
 // {
 //     console.log("Photography")
 // }
-// hobbies() // here invokes or call the function
+// hobbies(); // here invokes or call the function
 // Output : Photography 
 
 //-----------------------
@@ -39,39 +39,47 @@
 // nameOfTheFunction();
 // Woooh I am learning JavaScript
 
-//--------------------------------
+//-----------------------
+
 
 //FUNCTION USING PARAMETERS
+
 //***************************//
 
 //EXAPMLE 1
 
+// function FavIteam(food)
+// {
+//     console.log("my favourit item is " +food);
+// }
+// FavIteam("Icecream")
+// FavIteam("pizza");
+// FavIteam("Dosha");
+//output below
+//my favourit item is Icecream
+//my favourit item is pizza
+//my favourit item is Dosha
 //we can give an input from outside
+
+//-----------------------
+
+ //EXAMPLE 2
+
 // function heloparayuka(name) // here name is a parameter
 // {
 //     console.log("Hello Iam" +name);
 
 // }
-// heloparayuka("appu");
 // heloparayuka("Ranju");
+//output : Hello I am Ranju
 
 //-----------------------
 
-// //EXAPMLE 2
+// //EXAPMLE 3
 
 // function kuttuka(a,b)
 // {
 //     console.log(a+b);
-// }
-// kuttuka(5,10);
-
-//-----------------------
-
-//EXAPMLE 3
-
-// function kuttuka(a,b)
-// {
-//     console.log();
 // }
 // kuttuka(5,10);
 
@@ -84,21 +92,111 @@
 // console.log("I am" + age);
 // }
 // enteage(20);
-
-//-----------------------
-//EXAPMLE 5
-
-// function vehicle(bike,car,lory)
-// {
-//     console.log("enik" +lory);
-// }
-// vehicle("a", "bullet", "taxi");
+//Output : I am 20
 
 //----------------------
-//EXAPMLE 6
 
-// function vehicle(bike,car,lory,bus)
+// EXAMPLE 5
+
+// function OpenBox(toyname)
 // {
-//     console.log("enik" +bus + "ishttom");
+
+// console.log("I opend the box and found a toy name" + toyname);
 // }
-// vehicle("null", "null", "nayikurukkn","xyx");
+// OpenBox("Teddy Bear");
+// Output : mI opend the box and found a toy nameTeddy Bear
+
+//----------------------
+
+// EXAMPLE 6
+
+// function VisitZoo(name)
+// {
+//     console.log("We so an"+ name + "at the zoo");
+// }
+// VisitZoo("Elephant");
+//Output : We so anElephantat the zoo
+
+//----------------------
+
+// EXAMPLE 6
+
+// function SchoolBus(number)
+// {
+// console.log("I am sitting in bus number" +number);
+
+// }
+// SchoolBus(12)
+//Output : I am sitting in bus number12
+
+//----------------------
+
+// EXAMPLE 7
+
+// function DrinkGlass(drinkname)
+// {
+//     console.log(" Iam drinking a glass of "   +drinkname);
+
+// }
+// DrinkGlass("Water");
+// Output : Iam drinking a glass ofWater
+
+
+//FUNCTIONS USING MULTIPLE PARAMETERS
+
+ //***********************************//
+
+ //EXAMPLE 1
+// function StudentInfo(name, age) //pocket 1 is : name , pocket 2 is age both r empty pocket FCFS
+// {
+//     console.log(name + " is " + age + " years old " );
+// }
+// StudentInfo("Ranju","32");
+// StudentInfo("Jasir","25");
+// Ranju is 32 years old 
+// Jasir is 25 years old 
+
+//----------------------
+
+//EXAMPLE 2
+// function SchoolReport(studentname,grade)
+// {
+// console.log(studentname + " got" + grade + " in the exam");
+// }
+// SchoolReport("Rahul"," A");
+//Rahul got A in the exam
+
+//----------------------
+
+//EXAMPLE 3
+// function CricketScore(playername,runs)
+// {
+//     console.log(playername +"scored"+ runs + " runs today");
+// }
+// CricketScore("Sanju"," 100");
+//Sanjuscored 100 runs today
+
+//----------------------
+
+//EXAMPLE 3
+// function BuyItem(itemname,quantity,price)
+// {
+//     console.log("I bought" + quantity + itemname + " for" + price + " dollar ")
+// }
+// BuyItem(" Tomato,"," 3KILO"," 100");
+// I bought 3KILO Tomato, for 100 dollar 
+
+//----------------------
+
+// FUNCTION USING RETURN TYPE
+//*****************************//
+
+// function StrinfInfo()
+// {
+//     return "I am Ranju";
+// }
+// finalresult = StrinfInfo();
+// console.log(finalresult);
+//Output : ranju
+//purpose : logic + result 
+
